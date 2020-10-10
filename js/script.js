177142d2ebed1962ccd22e8c40e6a41324fd925c
praise-laurine <praiselaurine10@gmail.com>
2020-10-10 14:21:03 +0300

    add function

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $("#work4").mouseover(function(){
+        $("#black").show();
+    }).mouseout(function(){
+        $("#black").hide();
+    });
+
+    $("#work3").mouseover(function(){
+        $("#ontario").show();
+    }).mouseout(function(){
+        $("#ontario").hide();
+    });
+
+    $("#work2").mouseover(function(){
+        $("#pyramid").show();
+    }).mouseout(function(){
+        $("#pyramid").hide();
+    });
+
+    $("#work1").mouseover(function(){
+        $("#jim").show();
+    }).mouseout(function(){
+        $("#jim").hide();
+    });
+
+    $("#work5").mouseover(function(){
+        $("#nyc-day").show();
+    }).mouseout(function(){
+        $("#nyc-day").hide();
+    });
+
+    $("#work6").mouseover(function(){
+        $("#calc").show();
+    }).mouseout(function(){
+        $("#calc").hide();
+    });
+
+    $("#work7").mouseover(function(){
+        $("#burn").show();
+    }).mouseout(function(){
+        $("#burn").hide();
+    });
+
+    $("#work8").mouseover(function(){
+        $("#giraffe").show();
+    }).mouseout(function(){
+        $("#giraffe").hide();
+    });
+
+});
